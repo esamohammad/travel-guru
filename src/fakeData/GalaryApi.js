df500b7db2d6d6bd2e9c942413a934740eeccc96
esamohammad <esa.ce2020@gmail.com>
2023-04-01 16:40:14 +0600

    fakedata

---
+++ src/fakeData/GalaryApi.js
@@ -0,0 +1,49 @@
+export const hotels = [
+    {
+      id: 1,
+      name: "Ahasan Monjil",
+      photoUrl: "https://nomadparadise.com/wp-content/uploads/2021/04/bangladesh-places-01-768x512.jpg.webp",
+    },
+    {
+      id: 2,
+      name: "Sylhet",
+      photoUrl: "https://wander-lush.org/wp-content/uploads/2022/03/Most-eautiful-places-in-Bangladesh-WMC-Sylhet.jpg",
+    },
+    {
+      id: 3,
+      name: "Old Dhaka",
+      photoUrl: "https://wander-lush.org/wp-content/uploads/2022/03/Most-eautiful-places-in-Bangladesh-CP-Old-Dhaka.jpg",
+    },
+    {
+      id: 4,
+      name: "Satkhira",
+      photoUrl: "https://wander-lush.org/wp-content/uploads/2022/03/Most-eautiful-places-in-Bangladesh-CP-Satkhira.jpg",
+    },
+    {
+      id: 5,
+      name: "Sundorban",
+      photoUrl: "https://wander-lush.org/wp-content/uploads/2022/03/Most-eautiful-places-in-Bangladesh-CP-Sundarbans-NP.jpg",
+    },
+    {
+      id: 6,
+      name: "Sreemangal",
+      photoUrl: "https://wander-lush.org/wp-content/uploads/2022/03/Most-eautiful-places-in-Bangladesh-CP-Sreemangal.jpg",
+    },
+    {
+      id: 7,
+      name: "Keokradongl",
+      photoUrl: "https://wander-lush.org/wp-content/uploads/2022/03/Most-eautiful-places-in-Bangladesh-CP-Keokradong.jpg",
+    },
+    {
+      id: 8,
+      name: "Coxs Bazar",
+      photoUrl: "https://wander-lush.org/wp-content/uploads/2022/03/Most-eautiful-places-in-Bangladesh-US-Coxs-Bazar.jpg",
+    },
+    {
+      id: 9,
+      name: "St. Martins Island",
+      photoUrl: "https://wander-lush.org/wp-content/uploads/2022/03/Most-eautiful-places-in-Bangladesh-CP-St-Martins-Island.jpg",
+    }
+    
+  ]
+  
